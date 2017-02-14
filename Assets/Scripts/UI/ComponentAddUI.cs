331cf94ace83687a63fa93756cb1be4b3ea4cec0
MilindNilekani <mnilekan@andrew.cmu.edu>
2017-02-14 10:39:43 -0500

    Done with this for a while. Starting Bowling Green

--- Assets/Scripts/UI/ComponentAddUI.cs
+++ Assets/Scripts/UI/ComponentAddUI.cs
@@ -44,7 +44,7 @@ public class ComponentAddUI : MonoBehaviour, IBeginDragHandler,IDragHandler,IEnd
 				_grid.SetEldest (sib1.Position.x, sib1.Position.y, sib1.Position.z, eld.Position);
 				_grid.SetEldest (sib2.Position.x, sib2.Position.y, sib2.Position.z, eld.Position);
 
-			} else if(_typeOfItem!=GridHandler.SpotType.OR_CENTRE || _typeOfItem!=GridHandler.SpotType.AND_CENTRE){
+			} else if(_typeOfItem!=GridHandler.SpotType.OR_CENTRE && _typeOfItem!=GridHandler.SpotType.AND_CENTRE){
 				if (_grid.GetComponentType (pos.x, pos.y, pos.z) == GridHandler.SpotType.OR_CENTRE || _grid.GetComponentType(pos.x,pos.y,pos.z)==GridHandler.SpotType.AND_CENTRE) {
 					List<GridHandler.GridSpot> sib = _grid.GetSiblings (pos.x, pos.y, pos.z);
 					foreach (GridHandler.GridSpot s in sib) {
